8293a19831049aae29d72b185865cd802d6173ab
Manfred Karrer <mk@nucleo.io>
2018-03-31 22:07:34 -0500

    Refactorings and improvements in Proposal domain: - Removed signature from ProposalPayload - Only add hash of payload to OpReturn data not signature + hash - Rename getCreateCompensationRequestFee to getProposalFee - Rename CompensationAmountException to ValidationException - Move code from Restrictions to CompensationRequestConsensus - Rename RemoveAltcoinProposal to RemoveAssetProposal - Add version byte for PROPOSAL

--- src/main/java/bisq/common/app/Version.java
+++ src/main/java/bisq/common/app/Version.java
@@ -119,6 +119,7 @@ public class Version {
     }
 
     public static final byte COMPENSATION_REQUEST_VERSION = (byte) 0x01;
+    public static final byte PROPOSAL = (byte) 0x01;
     public static final byte BLIND_VOTE_VERSION = (byte) 0x01;
     public static final byte VOTE_REVEAL_VERSION = (byte) 0x01;
 }
