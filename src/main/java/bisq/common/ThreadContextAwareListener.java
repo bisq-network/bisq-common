788e227072c85d6cca7cc0658a2a5423c668729f
Manfred Karrer <mk@nucleo.io>
2018-04-14 11:57:45 -0500

    Change threading behaviour of ThreadContextAwareListeners
    - If executeOnUserThread is set call listeners directly without using executor.execute() to avoid that we need to wait until running task is complete
    - Dont use nodExecutor at readPersisted as that is called at startup so no need to change thread here
    - Remove getExecutor and execute default methods from ThreadContextAwareListener
    - Add executeOnUserThread default method to ThreadContextAwareListener
    - Add notifyHashMapChangedListenersOnRemove and notifyHashMapChangedListenersOnAdded to P2PDataStorage

--- src/main/java/bisq/common/ThreadContextAwareListener.java
+++ src/main/java/bisq/common/ThreadContextAwareListener.java
@@ -17,19 +17,14 @@
 
 package bisq.common;
 
-import java.util.concurrent.Executor;
-
 /**
  * Used for supporting to define the thread in which the listener is executed.
  */
 public interface ThreadContextAwareListener {
-    // Default executor used for calling the handlers is user thread.
-    // Listener who want to get called in a custom thread need to overwrite that method.
-    default Executor getExecutor() {
-        return UserThread.getExecutor();
-    }
-
-    default void execute(Runnable runnable) {
-        getExecutor().execute(runnable);
+    // If overwritten and false is returned the caller will map to the userThread, otherwise it will be called directly
+    // from the running thread. We do not user executor.execute() as we don't want to wait until the running task is
+    // completed.
+    default boolean executeOnUserThread() {
+        return true;
     }
 }
